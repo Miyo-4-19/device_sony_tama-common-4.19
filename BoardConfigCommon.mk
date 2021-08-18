410c4f65f264778fbca25d6cd687a65e5b501c10
PainKiller3 <ninadpatil100@gmail.com>
2021-08-18 19:42:47 +0530

    [DNM] tama-common: Go Permissive
    Change-Id: I8e11f26394419ba1400afbe365a1c2a51a0cd1bf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -70,6 +70,7 @@ BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += panic_on_err=1
 BOARD_KERNEL_CMDLINE += msm_drm.dsi_display0=somc,default_cmd_panel:config0
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm845
