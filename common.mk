5bae1bf86bad915f2a452c02981205625960bcf2
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-03-05 01:07:30 +0100

    tama-common: Build ANT+ HIDL interface library from source
    Change-Id: Ic6f14cc72a6fb8655b43ca89f7aa9cf977a0d795

--- common.mk
+++ common.mk
@@ -98,6 +98,10 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# ANT+
+PRODUCT_PACKAGES += \
+    com.dsi.ant@1.0.vendor
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
